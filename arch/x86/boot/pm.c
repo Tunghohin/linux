41e5609f3d6a8151326513861e1b6422d94541da
Tunghohin <houhin0809@gmail.com>
2026-04-22 01:35:25 +0800

    add out-of-tree modules, roll back pm.c

--- arch/x86/boot/pm.c
+++ arch/x86/boot/pm.c
@@ -120,8 +120,6 @@ void go_to_protected_mode(void)
 	/* Actual transition to protected mode... */
 	setup_idt();
 	setup_gdt();
-
-	puts("Entering protected mode...\n");
 	protected_mode_jump(boot_params.hdr.code32_start,
 			    (u32)&boot_params + (ds() << 4));
 }
